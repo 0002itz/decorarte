a8b9286bd242dab9c612ba8a0a6549c20758ddb0
itz <ortegagit@gmail.com>
2023-06-19 11:32:45 -0600

    creacion de views index

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    title= "titel_index"
+    return render(request,"index.html", {
+        'title': title
+    })
+
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
